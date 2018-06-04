20e767eacd861eb950e98dcd69e46c02fd2be373
aa1024 <11321158+aa1024@users.noreply.github.com>
2018-06-04 23:19:14 +0100

    ws.js

--- scripts/ws.js
+++ scripts/ws.js
@@ -1,1 +1,25 @@
  	console.log("Loading ws.js....");
+
+ 	var wsConn;
+
+ 	function connect() {
+ 	    if ("WebSocket" in window) {
+ 	        wsConn = new WebSocket("ws://localhost:2786/");
+ 	        console.log("Connecting to q process...");
+ 	        wsConn.onopen = function(arg) { console.log("Connection Open"); }
+ 	        wsConn.onclose = function(arg) {
+ 	            console.log("Disconnected");
+ 	            wsConn.close();
+ 	        }
+ 	        wsConn.onmessage = function(arg) {
+ 	            obj = JSON.parse(arg.data);
+ 	            console.log(obj)
+ 	            refresh(obj[0])
+ 	        }
+ 	        wsConn.onerror = function(arg) { console.log(arg.data); }
+ 	    } else alert("Browser does not support WebSockets");
+ 	}
+
+ 	function refresh(json) {
+		console.log(json);
+ 	}
